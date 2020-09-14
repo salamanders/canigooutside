88a23c7f5e81664865ea1657542fb6a436104c49
Benjamin Hill <benjaminhill@gmail.com>
2020-09-14 11:56:21 -0700

    No location?  No site for you.

--- public/js/polyfill.js
+++ public/js/polyfill.js
@@ -16,20 +16,9 @@ function sleep(ms) {
 const getPosition = async (modalId='position-permission') => {
     return new Promise((resolve, reject) => {
         navigator.geolocation.getCurrentPosition(resolve, (positionError)=>{
-            console.warn(`Issue getting position, trying modal.`, positionError);
-            const permissionElt = document.getElementById(modalId);
-            const permissionButton = permissionElt.getElementsByTagName('button')[0];
-            permissionButton.onclick = () => {
-                navigator.geolocation.getCurrentPosition(position => {
-                    try {
-                        resolve(position);
-                        permissionElt.close();
-                    } catch (e) {
-                        console.warn(`Issue closing modal`, e);
-                    }
-                }, reject);
-            };
-            permissionElt.showModal();
+            console.warn(`Issue getting position, displaying modal.`, positionError);
+            document.getElementById(modalId).showModal();
+            // Never resolves or rejects, which is ok.
         });
     });
 };
